b21bad39ab6c143073b9f706f5a75a48b75fd0d4
williycole <64283810+williycole@users.noreply.github.com>
2021-07-07 22:12:27 -0500

    Minor upgrades and gatsby upgrade
    Upgrading Gatsby and Mikes Bio Pic.

--- src/components/bio.js
+++ src/components/bio.js
@@ -37,8 +37,8 @@ const Bio = () => {
         layout="fixed"
         formats={["AUTO", "WEBP", "AVIF"]}
         src="../images/profile-pic.jpeg"
-        width={50}
-        height={50}
+        width={100}
+        height={100}
         quality={95}
         alt="Profile picture"
       />
@@ -46,6 +46,7 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
+          {/* need to fix this and link to mikes facebook */}
           {/* <a href={`https://twitter.com/${social?.twitter || ``}`}>
             You should follow them on Twitter
           </a> */}
